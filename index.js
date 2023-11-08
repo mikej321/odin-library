137107fbaf4a2aba0ec38863f15e768cbfff5a5a
Michael Johnson <michaeljcantley@gmail.com>
2023-11-08 11:02:11 -0500

    Changed UI & Implemented JS

--- index.js
+++ index.js
@@ -1,233 +1,278 @@
-// Global Variables
-
-const mainContainer = document.querySelector('.main-container');
+const content = document.querySelector('.main-content');
+const myForm = document.querySelector('.myForm');
+const add = document.querySelector('.add');
 const addBook = document.querySelector('.addBook');
-document.addEventListener('click', deleteCard);
-document.addEventListener('click', changeStatus);
-
-function deleteCard(event) {
-    // console.log(event.target.parentElement);
-
-    if (event.target.parentElement.classList.contains('card') && event.target.classList == 'delete') {
-        console.log(event.target);
-        let grabbedIndex = event.target.parentElement.dataset.listing;
-        for (let i = 0; i < myLibrary.length; i++) {
-            if (grabbedIndex == i) {
-                myLibrary.splice(`${grabbedIndex}`, 1);
-                console.log(event.target.parentElement);
-                event.target.parentElement.classList.add('disable');
-            }
-            
-        }
-    }
-}
-
-function changeStatus(event) {
-    if (event.target.parentElement.classList.contains('card') && event.target.classList == 'readButton') {
-        let grabbedIndex = event.target.parentElement.dataset.listing;
-        const readStatusElements = document.querySelectorAll('.elStatus');
-        if (readStatusElements[grabbedIndex].textContent.includes('yes')) {
-            readStatusElements[grabbedIndex].textContent = 'Read: no';
-        } else if (readStatusElements[grabbedIndex].textContent.includes('no')) {
-            readStatusElements[grabbedIndex].textContent = 'Read: yes';
-        }
-
-        myLibrary[grabbedIndex].readStatusChange();
-        
-    }
-}
-
-
-
-
-
-
-const display = document.querySelector('.displayLibrary').addEventListener('click', function() {
-    bookDisplay();
-});
-
-
-
-const form = document.querySelector('form');
-let authorInput = document.querySelector('#author');
-let titleInput = document.querySelector('#title');
-let pagesInput = document.querySelector('#pages');
-let statusInput = document.querySelector('#readStatus');
-const finish = document.querySelector('button[type="submit"]');
-let indexPosition = 0;
-
-let myAuthor;
-let myTitle;
-let myPages;
-let myStatus;
-
-
-/* write a function that loops through the array and displays each
-book on the page. I can display them in some sort of table, or
-each on their own 'card'. It might help for now to manually add
-a few books to my array so I can see the display */
-
-/* Add a 'new book' button that brings up a form allowing users
-to input the details for the new book: author, title, number of pages,
-whether it's been read, etc. Look up how to prevent events with event.preventDefault()
-for the submit button as it'll want to try to send to a server that doesn't exist */
-
-
-function bookDisplay() {
-    // loops through the array
-
-    // displays each book on the page
-    
-    // you can display them in some sort of table or on their own 'card'
-    
-    /* It might help for now to manually add a few books to your array
-    so you can see the display */
-    
-    for (let book of myLibrary) {
-        card = document.createElement('div');
-        card.classList.add('card');
-        
-        let elAuthor = document.createElement('p');
-        elAuthor.textContent = 'Author: ';
-        let spanAuthor = document.createElement('span');
-        spanAuthor.textContent = book.author;
-        elAuthor.append(spanAuthor);
-        
-        let elTitle = document.createElement('p');
-        elTitle.textContent = 'Title: ';
-        let spanTitle = document.createElement('span');
-        spanTitle.textContent = book.title;
-        elTitle.append(spanTitle);
-        
-        let elPages = document.createElement('p');
-        elPages.textContent = 'Pages: ';
-        let spanPages = document.createElement('span');
-        spanPages.textContent = book.pages;
-        elPages.append(spanPages);
-        
-        let elStatus = document.createElement('p');
-        elStatus.classList.add('elStatus')
-        elStatus.textContent = 'Read: ';
-        let spanStatus = document.createElement('span');
-        spanStatus.textContent = book.readStatus;
-        elStatus.append(spanStatus);
-        
-        let deleteButton = document.createElement('button');
-        deleteButton.classList.add('delete');
-        deleteButton.textContent = 'Remove';
-
-        let readStatusButton = document.createElement('button');
-        readStatusButton.classList.add('readButton');
-        readStatusButton.textContent = 'Read?';
-        
-        card.dataset.listing = `${indexPosition++}`;
-
-        mainContainer.insertAdjacentElement('beforeend', card);
-        card.insertAdjacentElement('beforeend', elAuthor);
-        card.insertAdjacentElement('beforeend', elTitle);
-        card.insertAdjacentElement('beforeend', elPages);
-        card.insertAdjacentElement('beforeend', elStatus);
-        card.insertAdjacentElement('beforeend', readStatusButton);
-        card.insertAdjacentElement('beforeend', deleteButton);
-
-        deleteButton.addEventListener('click', function() {
-            // console.log(this.parentElement.dataset.listing);
-            
-        })
-    }
-
-    
-   
-}
-
-
-
-
+const removeBook = document.querySelector('.remove');
+const formEl = document.querySelectorAll('.formEl input');
 
 let myLibrary = [];
+const alphaRegex = /^[a-zA_Z0-9 ]*$/;
 
-function Book(author, title, pages, readStatus) {
-    // constructor function
-    this.author = author;
+function Book(title, author, pages, haveRead) {
     this.title = title;
+    this.author = author;
     this.pages = pages;
-    this.readStatus = readStatus;
+    this.haveRead = haveRead;
 }
-
 
 Book.prototype = {
-    
-    addBook(author, title, pages, readStatus) {
-        author = authorInput.value;
-        title = titleInput.value;
-        pages = pagesInput.value;
-        readStatus = statusInput.value;
-        let newBook = new Book(author, title, pages, readStatus.toLowerCase());
-        myLibrary.push(newBook);
+    setBook: function() {
+        const myTitle = document.querySelector('#title').value;
+        this.title = myTitle;
+        const myAuthor = document.querySelector('#author').value;
+        this.author = myAuthor;
+        const myPages = document.querySelector('#pages').value;
+        this.pages = myPages;
+        const read = document.querySelector('#read').checked;
+        this.haveRead = read;        
     },
-
-    readStatusChange() {
-        
-        
-
-        // if (this.readStatus == 'yes') { 
-        //     debugger;
-        //     this.readStatus = 'no';
-        // } else if (this.readStatus == 'no') {
-        //     this.readStatus = 'yes';
-        // }
-
     
+    addBookToLibraryArr: function() {
+        let newBook = new Book(this.title, this.author, this.pages, this.haveRead);
+        myLibrary.push(newBook);
+    }
+    
+}
+
+
+
+
+function bringUpForm(event) {
+    event.preventDefault();
+    myForm.classList.toggle('visible');
+    let intervalID = setTimeout(() => {
+        const cards = document.querySelectorAll('.card');
+        if (cards === null) {
+            console.log('I am null');
+        } else if (cards) {
+            cards.forEach(card => card.classList.add('phaseOut'));
+        }
+        clearInterval(intervalID);
+    }, 10);
+    if (myForm.classList.contains('visible')) {
+        add.textContent = 'Cancel';
+    }
+
+    if (!myForm.classList.contains('visible')) {
+        add.textContent = 'Add';
     }
 }
 
-addBook.addEventListener('click', function() {
-    // change form display from none to block
-
-    if (!form.classList.contains('open')) {
-        form.classList.toggle('open');
-        addBook.textContent = 'Cancel';
-    } else {
-        form.classList.remove('open');
-        addBook.textContent = 'Add Book';
-        authorInput.value = '';
-        titleInput.value = '';
-        pagesInput.value = '';
-        statusInput.value = '';
-
+function changeAddBack() {
+    if (add.textContent === 'Cancel') {
+        add.textContent = 'Add';
+    } else if (add.textContent === 'Add') {
+        add.textContent = 'Cancel'
     }
-})
+}
 
-// finish.addEventListener('click', function(event) {
-//     event.preventDefault();
-// })
+function hideCards() {
 
-finish.addEventListener('click', function(event) {
-    event.preventDefault();
-    Book.prototype.addBook();
-    form.classList.remove('open');
-    addBook.textContent = 'Add Book';
-    authorInput.value = '';
-    titleInput.value = '';
-    pagesInput.value = '';
-    statusInput.value = '';
-})
+}
 
-
-
-
-
+function closeForm() {
+    myForm.classList.remove('visible');
+    let intervalID = setTimeout(() => {
+        const cards = document.querySelectorAll('.card');
+        if (cards === null) {
+            console.log('I am null');
+        } else if (cards) {
+            cards.forEach(card => card.classList.remove('phaseOut'));
+        }
+    })
+}
 
 function addBookToLibrary() {
-    // do stuff here
-    
+    Book.prototype.setBook();
+    Book.prototype.addBookToLibraryArr();
+    closeForm();
+    renderEl();
+}
+
+let indexPosition = 0;
+
+function removeEle(event) {
+   let isItRemoveButton = event.target;
+   if (isItRemoveButton.tagName === 'BUTTON' && isItRemoveButton.classList.contains('remove')) {
+    let parentEle = isItRemoveButton.parentElement.parentElement;
+    removePosition = isItRemoveButton.parentElement.parentElement.getAttribute('data-listing');
+    removePosition = Number(removePosition);
+    myLibrary.splice(removePosition, 1);
+    parentEle.remove();
+    indexPosition--;
+   }
+}
+
+function changeEle(event) {
+    let isItChangeButton = event.target;
+    if (isItChangeButton.tagName === 'BUTTON' && isItChangeButton.classList.contains('change')) {
+        let changePosition = isItChangeButton.parentElement.getAttribute('data-Listing');
+        changePosition = Number(changePosition);
+        if (myLibrary[changePosition].haveRead === true) {
+            myLibrary[changePosition].haveRead = false;
+        } else {
+            myLibrary[changePosition].haveRead = true;
+        }
+
+        if (myLibrary[changePosition].haveRead === true) {
+            isItChangeButton.parentElement.parentElement.children[3].children[0].textContent = 'Yes';            
+        } else {
+            isItChangeButton.parentElement.parentElement.children[3].children[0].textContent = 'No';
+        }
+    }
 }
 
 
+function renderEl() {
 
-/* Object.entries() can be used to create a two-element subarray
-   of all of the properties on an object. */
+  
+   for (let i = indexPosition; i < myLibrary.length; i++) {
+    const myEl = document.createElement('div');
+    myEl.classList.add('card');
+    
+    const cardTitle = document.createElement('p');
+    cardTitle.textContent = 'Title: ';
+    const titleSpan = document.createElement('span');
+    titleSpan.textContent = myLibrary[i].title;
+    cardTitle.append(titleSpan);
+    myEl.append(cardTitle);
 
-// Object.fromEntries() creates a new object from a list of entries 
+    const cardAuthor = document.createElement('p');
+    cardAuthor.textContent = 'Author: ';
+    const authorSpan = document.createElement('span');
+    authorSpan.textContent = myLibrary[i].author;
+    cardAuthor.append(authorSpan);
+    myEl.append(cardAuthor);
+
+    const cardPages = document.createElement('p');
+    cardPages.textContent = 'Pages: ';
+    const pagesSpan = document.createElement('span');
+    pagesSpan.textContent = myLibrary[i].pages;
+    cardPages.append(pagesSpan);
+    myEl.append(cardPages);
+
+    const cardRead = document.createElement('p');
+    cardRead.textContent = 'Read: ';
+    const readSpan = document.createElement('span');
+   
+    if (myLibrary[i].haveRead === true) {
+        readSpan.textContent = 'Yes';
+    } else {
+        readSpan.textContent = 'No';
+    }
+
+    cardRead.append(readSpan);
+    myEl.append(cardRead);
+
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('buttonContainer');
+    myEl.append(buttonContainer);
+    
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove');
+    removeButton.textContent = 'Remove';
+    buttonContainer.append(removeButton);
+
+    const changeButton = document.createElement('button');
+    changeButton.classList.add('change');
+    changeButton.textContent = 'Status';
+    buttonContainer.append(changeButton);
+    
+    myEl.dataset.listing = `${indexPosition++}`;
+    
+
+    content.append(myEl);
+
+   }
+
+}
+
+function checkInput() {
+    myForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const myTitle = document.querySelector('.formEl > #title');
+        const myAuthor = document.querySelector('.formEl > #author');
+        const myPages = document.querySelector('.formEl > #pages');
+        const haveIRead = document.querySelector('.formEl > #read');
+        const titleError = document.querySelector('.titleError');
+        const pagesError = document.querySelector('.pagesError');
+        const authorError = document.querySelector('.authorError');
+        const checkedError = document.querySelector('.checkError');
+        const errorMessage = document.querySelector('.errorMessage');
+
+        if (myTitle.validity.valueMissing) {
+            if (myTitle.classList.contains('inputValid')) {
+                myTitle.classList.remove('inputValid');
+            }
+            titleError.parentElement.dataset.active = true;
+            titleError.textContent = 'Please enter a title';
+            titleError.classList.add('errorP');
+            myTitle.classList.add('error');
+        } else if (!myTitle.validity.valueMissing) {
+            if (myTitle.classList.contains('error')) {
+                titleError.classList.remove('errorP');
+                myTitle.classList.remove('error');
+            }
+            titleError.parentElement.dataset.active = false;
+            titleError.textContent = '';
+            myTitle.classList.add('inputValid');
+        }
+
+        if (myAuthor.validity.valueMissing) {
+            if (myAuthor.classList.contains('inputValid')) {
+                myAuthor.classList.remove('inputValid');
+            }
+            authorError.parentElement.dataset.active = true;
+            authorError.textContent = 'Please enter an author\'s name';
+            authorError.classList.add('errorP');
+            myAuthor.classList.add('error');
+        } else if (!myAuthor.validity.valueMissing) {
+            if (myAuthor.classList.contains('error')) {
+                authorError.classList.remove('errorP');
+                myAuthor.classList.remove('error');
+            }
+            authorError.parentElement.dataset.active = false;
+            authorError.textContent = '';
+            myAuthor.classList.add('inputValid');
+        }
+
+        if (myPages.validity.valueMissing) {
+            if (myAuthor.classList.contains('inputValid')) {
+                myAuthor.classList.remove('inputValid');
+            }
+            pagesError.parentElement.dataset.active = true;
+            pagesError.textContent = 'Please provide the # of pages';
+            pagesError.classList.add('errorP');
+            myPages.classList.add('error');
+        } else if (!myPages.validity.valueMissing) {
+            if (myAuthor.classList.contains('error')) {
+                pagesError.classList.remove('errorP');
+                myPages.classList.remove('error');
+            }
+            pagesError.parentElement.dataset.active = false;
+            pagesError.textContent = '';
+            myPages.classList.add('inputValid');
+        }
+
+        if (!myTitle.validity.valueMissing && !myAuthor.validity.valueMissing && !myPages.validity.valueMissing) {
+            addBookToLibrary();
+            changeAddBack();
+            myTitle.classList.remove('inputValid');
+            myTitle.value = '';
+            myAuthor.classList.remove('inputValid');
+            myAuthor.value = '';
+            myPages.classList.remove('inputValid');
+            myPages.value = '';
+        }
+
+        
+    })
+}
+
+add.addEventListener('click', bringUpForm);
+checkInput();
+// addBook.addEventListener('click', addBookToLibrary);
+document.addEventListener('click', removeEle);
+document.addEventListener('click', changeEle);
 
 
